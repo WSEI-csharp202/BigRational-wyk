000b398249a08c256bd407ae69b72bcac55c4ece
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2025-03-21 19:04:13 +0100

    Equality

--- BigRationalConsoleApp/Program.cs
+++ BigRationalConsoleApp/Program.cs
@@ -2,3 +2,8 @@
 
 var u = new BigRational(1, -2);
 Console.WriteLine(u);
+
+var w = new BigRational(-3, 6);
+Console.WriteLine(w);
+
+Console.WriteLine(u == w);
--- BigRationalLib/BigRationalEquals.cs
+++ BigRationalLib/BigRationalEquals.cs
@@ -1,6 +1,29 @@
 ﻿namespace BigRationalLib;
 
-public readonly partial struct BigRational
+public readonly partial struct BigRational : IEquatable<BigRational>
 {
+    public bool Equals(BigRational other)
+    {
+        return Numerator == other.Numerator &&
+               Denominator == other.Denominator;
+    }
 
+    override public bool Equals(object? obj)
+    {
+        return obj is BigRational other && Equals(other);
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Numerator, Denominator);
+    }
+
+    public static bool operator ==(BigRational left, BigRational right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(BigRational left, BigRational right)
+    {
+        return !left.Equals(right);
+    }
 }
